d007aa71c457e6c5c23168a6c919d7aa491ab701
J K <jkim65@buffalo.edu>
2020-10-26 11:39:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,8 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      params[:pirate]
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
     end
   end
 end
