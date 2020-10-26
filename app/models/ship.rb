d2bdc39741745606a97c3d13cf9a4cecc6dd05f0
J K <jkim65@buffalo.edu>
2020-10-26 11:46:24 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -4,6 +4,13 @@ class Ship
 
   SHIPS = []
 
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    SHIPS << self
+  end
+
   def self.all
     SHIPS
   end
